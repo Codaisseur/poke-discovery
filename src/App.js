cabcb432b94461887cb7b6f279e78c2f85c30f75
Swendude <swenmulderij@gmail.com>
2022-01-07 11:49:41 +0100

    Added detailpage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import "./App.css";
-import {Routes, Route, NavLink} from 'react-router-dom';
-import PokemonDiscoveryPage from './Pages/PokemonDiscoveryPage';
+import { Routes, Route, NavLink } from "react-router-dom";
+import PokemonDiscoveryPage from "./Pages/PokemonDiscoveryPage";
+import PokemonDetailPage from "./Pages/PokemonDetailPage";
 
 function App() {
   return (
     <div className="App">
       <h1>◓ Pokemon Discovery ◓</h1>
       <div className="AppNav">
-        <NavLink className='NavElement' end to="/">Home</NavLink>
+        <NavLink className="NavElement" end to="/">
+          Home
+        </NavLink>
       </div>
       <Routes>
-        <Route path="/" element={<PokemonDiscoveryPage/>} />
+        <Route path="/" element={<PokemonDiscoveryPage />} />
+        <Route path="/details/:pokemon_name" element={<PokemonDetailPage />} />
       </Routes>
     </div>
   );
