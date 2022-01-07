7f6f6ee2ddcdf473078d3fb6e7d2213e202e0a9e
Swendude <swenmulderij@gmail.com>
2022-01-07 13:30:29 +0100

    Added filtering

--- src/Pages/PokemonDiscoveryPage.js
+++ src/Pages/PokemonDiscoveryPage.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-
+import { useNavigate, useParams } from "react-router-dom";
 import PokemonBlock from "../Components/PokemonBlock";
 
 function PokemonDiscoveryPage() {
   const [pokeList, setPokeList] = useState(null);
-
+  const [filter, setFilter] = useState("");
+  
   useEffect(() => {
     async function getPokemons() {
       const pokeResponse = await axios.get(
@@ -15,10 +16,16 @@ function PokemonDiscoveryPage() {
     }
     getPokemons();
   }, []);
+
+  const updateFilter = (e) => {
+    setFilter(e.target.value);
+  }
+
   return (
     <div>
+      <input type='text' value={filter} onChange={updateFilter}/>
       {pokeList ? (
-        pokeList.map((pokeObj) => <PokemonBlock name={pokeObj.name} />)
+        pokeList.filter((pokeObj) => pokeObj.name.startsWith(filter)).map((pokeObj, i) => <PokemonBlock key={i} name={pokeObj.name} />)
       ) : (
         <p>Loading ..</p>
       )}
